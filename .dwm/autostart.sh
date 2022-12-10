6ddd4545573fd18fc57a551eff0b3caa1df49303
henryqingmo <henryqingmo@gmail.com>
2022-12-10 16:29:46 +1100

    added picom

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,1 +1,9 @@
-xrandr --output eDP-1 --off
+## Turn off laptop display
+xrandr --output eDP-1 --off &
+## Restore wallpaper
+nitrogen --restore &
+## Start picom animation
+picom -b &
+## Starts notifications
+dunst &
+
